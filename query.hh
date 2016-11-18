3607403b7d093e056d1a5daa31125ff352321317
Lily Tsai <tslilyai@gmail.com>
2016-11-18 08:40:44 -0500

    oops forgot to add files

---
+++ query.hh
@@ -0,0 +1,20 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include "helpers.hh"
+
+/*
+    SUPPORTED QUERIES:
+    direct_ancestor(identifier) => return identifier
+    all_ancestors(identifier) => return list of identifiers
+    all_descendants(identifier) => return list of identifiers
+    all_paths(source, sink) => return list of list of identifiers
+    friends(identifier) => return list of identifiers (is this a useful query to support?)
+    metadata(identifier) => return (JSON output?) of identifier
+ */
+
+class Metadata;
+class Querier;
+class CompressedQuerier;
+
+#endif /* QUERY_H */

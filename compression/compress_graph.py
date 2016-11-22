3fcf97404f18fb1aa9446953932d8a37b771b91d
Aaron Bembenek <aaron.bembenek@gmail.com>
2016-11-22 18:38:10 -0500

    Added another sample compressed graph.

--- compression/compress_graph.py
+++ compression/compress_graph.py
@@ -120,6 +120,20 @@ class BasicCompressor(Compressor):
 
 def main():
     if len(sys.argv) < 2:
+        # Tiny DAG
+        #           2
+        #          / \
+        #         4   3   7
+        #        / \ /    |
+        #       5   0     6
+        #           |
+        #           1
+        # (all edges point from bottom up)
+        g = {1:[0], 0:[4,3], 2:[], 3:[2], 4:[2], 5:[4], 6:[7], 7:[]}
+        g = {v:[pj.Edge(e, None) for e in edges] for v, edges in g.items()}
+        metadata = {}
+
+        # Tiny tree
         #         2
         #         |
         #         4   3   7
@@ -128,9 +142,9 @@ def main():
         #           |
         #           1
         # (all edges point from bottom up)
-        g = {1:[0], 0:[4,3], 2:[], 3:[], 4:[2], 5:[4], 6:[7], 7:[]}
-        g = {v:[pj.Edge(e, None) for e in edges] for v, edges in g.items()}
-        metadata = {}
+        #g = {1:[0], 0:[4,3], 2:[], 3:[], 4:[2], 5:[4], 6:[7], 7:[]}
+        #g = {v:[pj.Edge(e, None) for e in edges] for v, edges in g.items()}
+        #metadata = {}
     else:
         g, metadata = pj.json_to_graph_data(sys.argv[1])
     c = BasicCompressor(preprocess.BfsPreprocessor(g, metadata))

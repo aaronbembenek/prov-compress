82173dd01c9220c8a30112fbca5edb6da1109d07
Aaron Bembenek <aaron.bembenek@gmail.com>
2016-11-24 12:23:56 -0500

    Some tweaks to graph_v1.

--- querier/graph_v1.hh
+++ querier/graph_v1.hh
@@ -10,14 +10,14 @@ class Graph_V1 : public Graph {
     public:
         Graph_V1(std::string&);
         ~Graph_V1();
-        std::vector<Node_Id> get_outgoing_edges(Node_Id);
-        size_t get_node_count();
+        std::vector<Node_Id> get_outgoing_edges(Node_Id) override;
+        size_t get_node_count() override;
     private:
         BitSet data;
         size_t* index;
         size_t index_length;
-        size_t nbits_for_degree;
-        size_t nbits_for_delta;
+        size_t nbits_degree;
+        size_t nbits_delta;
         size_t base_pos;
 };
 

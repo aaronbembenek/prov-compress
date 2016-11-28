a9532dd43308ada4c477fa2a9f88cbe1f9ca74eb
Lily Tsai <tslilyai@gmail.com>
2016-11-28 15:06:47 -0500

    add script to benchmarks metadata query performance

--- benchmarks/get_compression_results.sh
+++ benchmarks/get_compression_results.sh
@@ -1,10 +1,15 @@
-for f in results/*; do
-    cd ../compression && time (./main.py ../benchmarks/$f)
+file=results/compression_results.out
+rm $file
+touch $file
+
+for f in results/*.prov; do
+    (echo $f) >> results/compression_results.out 2>&1
+    cd ../compression && (time (./main.py ../benchmarks/$f)) >> ../benchmarks/$file 2>&1
     cd ../benchmarks
     csize=$(ls -lrt ../compression/compressed_metadata.txt ../compression/identifiers.txt | awk '{ total += $5 }; END { print total }')
     xz -9 $f 
     xzsize=$(ls -lrt $f.xz | awk '{ total += $5 }; END { print total }')
     xz -d $f.xz
     original=$(ls -lrt $f | awk '{ total += $5 }; END { print total }')
-    echo $csize $xzsize $original $(bc <<<"scale=2;$original/$csize") $(bc <<<"scale=2;$original/$xzsize") 
+    (echo $csize $xzsize $original $(bc <<<"scale=2;$original/$csize") $(bc <<<"scale=2;$original/$xzsize")) >> $file 2>&1
 done

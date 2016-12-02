72fb1e623b44aee5ded3ccd50511762a59ddaa5b
Lily Tsai <tslilyai@gmail.com>
2016-12-02 15:27:26 -0500

    fix bash variable addition

--- benchmarks/get_compression_results.sh
+++ benchmarks/get_compression_results.sh
@@ -6,13 +6,13 @@ rm $outfile
 touch $outfile
 
 for f in results/*.prov; do
-    (echo $f) >> results/compression_results.out 2>&1
+    (echo $f) >> $outfile 2>&1
     cd ../compression && ./main.py ../benchmarks/$f  >> ../benchmarks/$outfile 2>&1 
     xz -9 -k $idfile
     mdsize=$(ls -lrt $metafile | awk '{ total += $5 }; END { print total }')
     idsize=$(ls -lrt $idfile.xz | awk '{ total += $5 }; END { print total }')
     graphsize=$(ls -lrt $graphfile | awk '{ total += $5 }; END { print total }')
-    csize=($mdsize + $idsize + $graphsize)
+    csize=$(($mdsize + $idsize + $graphsize))
     rm *.xz
     cd ../benchmarks
     xz -9 $f 

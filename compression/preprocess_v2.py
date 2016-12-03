9d02798ef0f216028750751c62f25f80f0b2868e
Aaron Bembenek <aaron.bembenek@gmail.com>
2016-12-03 16:26:32 -0500

    Number nodes and identifiers.

--- compression/preprocess_v2.py
+++ compression/preprocess_v2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+from collections import deque
 from graph import Graph
 import json
 import sys
 from unionfind import CustomUnionFind
-from util import warn
+from util import nbits_for_int, warn
 
 RELATION_TYPS = {
     "wasGeneratedBy": ("prov:entity", "prov:activity"),
@@ -35,6 +36,9 @@ def clean_camflow_json(messy_json):
     return clean
 
 
+def is_version_edge(edge, metadata):
+    return metadata[edge.label].data["cf:type"] == "version"
+
 class PreprocessorV2:
 
     def __init__(self, json_obj):
@@ -47,7 +51,7 @@ class PreprocessorV2:
 
         graph = self._process_json()
         (collapsed, uf) = self._collapse_versions(graph)
-        order = self._get_visiting_order(collapsed)
+        self._number_identifiers(graph, collapsed, uf)
 
         self.initialized = True
 
@@ -107,7 +111,7 @@ class PreprocessorV2:
         uf = CustomUnionFind(vs)
         for v in vs:
             for edge in graph.get_outgoing_edges(v):
-                if self.metadata[edge.label].data["cf:type"] == "version":
+                if is_version_edge(edge, self.metadata):
                     uf.union(edge.dest, v)
         edge_map = {}
         collapsed = Graph()
@@ -123,45 +127,111 @@ class PreprocessorV2:
                     collapsed.add_edge(v, u, None)
         return (collapsed, uf)
 
-    # XXX this is going to be pretty memory-intensive...
-    def _get_visiting_order(self, graph):
-        reachable_map = {} 
-        for v in graph.get_vertices():
-            if v in reachable_map:
-                continue
-            local_visited = set([v])
-            stack = [(v, 0, set([v]))]
-            forwards = True
-            acc = set() 
-            while stack:
-                assert not forwards or not len(acc)
-                (v, i, local_acc) = stack[-1]
-                assert forwards or v not in reachable_map
-                if not forwards:
-                    local_acc.update(acc)
-                    acc = set()
-                elif v in reachable_map:
-                    forwards = False
-                    acc = reachable_map[v]
-                    stack.pop()
-                    continue
+    def _number_identifiers(self, graph, collapsed, uf):
+        sizes = {x:uf.get_size(x) for x in uf.leaders()}
+        ranker = TransposeBfsRanker(graph, collapsed, self.metadata, sizes)
+        id_map = ranker.rank()
 
-                edges = graph.get_outgoing_edges(v)
-                if i < len(edges):
-                    forwards = True
-                    stack[-1] = (v, i + 1, local_acc)
-                    dest = edges[i].dest
-                    if dest not in local_visited:
-                        stack.append((dest, 0, set([dest])))
-                        local_visited.add(dest)
-                else:
-                    forwards = False
-                    stack.pop()
-                    reachable_map[v] = local_acc
-                    acc = local_acc
-        return sorted(reachable_map.keys(),
-                      key=lambda v: -len(reachable_map[v]))
+        node_count = len(id_map)
+        node_bits = nbits_for_int(node_count)
+        for _id, entry in self.metadata.items():
+            if entry.typ in RELATION_TYPS:
+                head = entry.data[RELATION_TYPS[entry.typ][0]]
+                tail = entry.data[RELATION_TYPS[entry.typ][1]]
+                id_map[_id] = ((id_map[head] << node_bits)
+                               + id_map[tail] + node_count)
+        sorted_ids = sorted(id_map.keys(), key=lambda v: id_map[v])
+        with open("identifiers.txt", 'wb') as f:
+            f.write(node_count.to_bytes(4, byteorder='big')) 
+        with open("identifiers.txt", 'a') as f:
+            for _id in sorted_ids:
+                f.write(_id + ",")
+        self.id_map = id_map
 
+
+class TransposeBfsRanker:
+
+    def __init__(self, graph, collapsed, metadata, sizes):
+        self.graph = graph
+        self.collapsed = collapsed
+        self.metadata = metadata
+        self.sizes = sizes
+
+    def rank(self):
+        order = self._get_visiting_order()
+        rank = 0
+        self.rankings = {}
+        q = deque()
+        for v in order:
+            if v not in self.rankings:
+                q.append(v)
+            while q:
+                v = q.popleft()
+                rank = self._rank_collapsed(v, rank)
+                dests = [e.dest for e in self.collapsed.get_incoming_edges(v)]
+                for d in dests:
+                    if d not in self.rankings:
+                        q.append(d)
+        return self.rankings
+
+    def _rank_collapsed(self, v, rank):
+        cur = [v]
+        while cur:
+            cur = cur[0]
+            assert cur not in self.rankings
+            self.rankings[cur] = rank
+            rank += 1
+            cur = [e.dest for e in self.graph.get_incoming_edges(cur)
+                   if is_version_edge(e, self.metadata)]
+            assert len(cur) < 2
+        return rank 
+
+    # XXX placeholder... but maybe will be good, depending on how CamFlow
+    # assigns identifiers?
+    def _get_visiting_order(self):
+        return sorted(self.collapsed.get_vertices())
+
+    # XXX this terminates in the presence of cycles but does not assign scores
+    # correctly. To fix this, need to collapse connected components and run
+    # algorithm on that graph.
+#    def _get_visiting_order(self):
+#        scores = {} 
+#        for v in self.graph.get_vertices():
+#            if v in scores:
+#                continue
+#            local_visited = {v}
+#            stack = [(v, 0, 1)]
+#            forwards = True
+#            acc = 0 
+#            while stack:
+#                assert not forwards or not acc
+#                (v, i, local_acc) = stack[-1]
+#                assert forwards or v not in scores
+#                if not forwards:
+#                    local_acc += acc
+#                    stack[-1] = (v, i, local_acc) 
+#                    acc = 0
+#                elif v in scores:
+#                    forwards = False
+#                    acc = scores[v]
+#                    stack.pop()
+#                    continue
+#
+#                edges = self.graph.get_outgoing_edges(v)
+#                if i < len(edges):
+#                    forwards = True
+#                    stack[-1] = (v, i + 1, local_acc)
+#                    dest = edges[i].dest
+#                    if dest not in local_visited:
+#                        stack.append((dest, 0, 1))
+#                        local_visited.add(dest)
+#                else:
+#                    forwards = False
+#                    stack.pop()
+#                    scores[v] = local_acc
+#                    acc = local_acc
+#        print(scores)
+#        return sorted(scores.keys(), key=lambda v: -scores[v])
 
 def main():
     with open("example.json") as f:
